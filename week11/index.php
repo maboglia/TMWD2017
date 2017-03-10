1e42144552fdbad8785195eda936a76b9320ec26
maboglia <mauro.bogliaccino@gmail.com>
2017-03-10 11:07:31 +0100

    agg

---
+++ week11/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title></title>
+<style type="text/css">
+h1 {color: red;}
+</style>
+
+</head>
+<body>
+	<h1>benvenuti</h1>
+	
+	
+
+	<script type="text/javascript">console.log('benvenuto');</script>
+</body>
+</html>
